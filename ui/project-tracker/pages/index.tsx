11bbae919c4cff61ed360789b35f0dd998b55ee4
Balint Morasz <balint@morasz.hu>
2021-12-19 14:14:44 +0100

    feat(ui) create next.js ui

---
+++ ui/project-tracker/pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Project Tracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Project Tracker
+        </h1>
+
+          
+        
+      </main>
+
+      {/*<footer className={styles.footer}>*/}
+      {/*</footer>*/}
+    </div>
+  )
+}
+
+export default Home
