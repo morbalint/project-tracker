3cf8fcac23da5114f0cfca7a2bccbc7d4ca41971
Balint Morasz <balint@morasz.hu>
2021-12-21 09:47:59 +0100

    feat(ui) fetch all projects

--- ui/project-tracker/pages/index.tsx
+++ ui/project-tracker/pages/index.tsx
@@ -2,8 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then((res) => res.text())
 
 const Home: NextPage = () => {
+    
+    // TODO: use some dev env switch
+    const { data, error } = useSWR('http://localhost:8080/projects', fetcher)
+
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+    
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +26,10 @@ const Home: NextPage = () => {
           Project Tracker
         </h1>
 
-          
+          {!data && <p>loading...</p>}
+          {error && <p> error </p>}
+
+          {data && <p>{data}</p>}
         
       </main>
 
